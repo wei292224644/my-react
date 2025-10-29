fe236dc4d2c54b9144af64c6c013c5fffa3f2376
wwj <wei292224644@live.com>
2025-10-29 17:41:58 +0800

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: { ecmaVersion: 'latest', sourceType: 'script' }
+    }
+  },
+  tseslint.configs.recommended,
+  prettierRecommended
+]);
